769eeb4ede2a32a2ef17c2616f9314e443e60780
JoFiN <jofinjohn123@gmail.com>
2023-10-14 14:36:46 +0530

    rick and morty loading

--- src/Rick-and-Morty.js
+++ src/Rick-and-Morty.js
@@ -9,9 +9,11 @@ const Button = styled.button`
 
 export function RickMorty() {
   const [view, setView] = useState("characters");
+  const [loading, setLoading] = useState("true");
 
   useEffect(() => {
     async function fetchData() {
+      setLoading("false");
       const res = await fetch(
         `https://api.sampleapis.com/rickandmorty/${view}`,
         {
@@ -19,6 +21,7 @@ export function RickMorty() {
         }
       );
       const fetchData = await res.json();
+      setLoading("true");
       setData(fetchData);
     }
     fetchData();
@@ -54,100 +57,103 @@ export function RickMorty() {
           Locations
         </Button>
       </div>
+      {loading === "true" ? (
+        <div className="wrap__window">
+          {view === "characters"
+            ? data.map((i) => {
+                return (
+                  <div className="card">
+                    <p>
+                      <span>id: </span>
+                      <span>{i.id}</span>
+                    </p>
+                    <p>
+                      <span>Name:</span>
+                      <span> {i.name}</span>
+                    </p>
+                    <p>
+                      <span>Status:</span>
+                      <span> {i.status}</span>
+                    </p>
+                    <p>
+                      <span>Species: </span>
+                      <span>{i.species}</span>
+                    </p>
+                    <p>
+                      <span>Type: </span>
+                      <span>{i.type}</span>
+                    </p>
+                    <p>
+                      <span>Gender: </span>
+                      <span>{i.gender}</span>
+                    </p>
+                    <p>
+                      <span>Origin:</span>
+                      <span> {i.origin}</span>
+                    </p>
+                    <img src={i.image} alt={i.name} width={200} height={200} />
+                  </div>
+                );
+              })
+            : null}
 
-      <div className="wrap__window">
-        {view === "characters"
-          ? data.map((i) => {
-              return (
-                <div className="card">
-                  <p>
-                    <span>id: </span>
-                    <span>{i.id}</span>
-                  </p>
-                  <p>
-                    <span>Name:</span>
-                    <span> {i.name}</span>
-                  </p>
-                  <p>
-                    <span>Status:</span>
-                    <span> {i.status}</span>
-                  </p>
-                  <p>
-                    <span>Species: </span>
-                    <span>{i.species}</span>
-                  </p>
-                  <p>
-                    <span>Type: </span>
-                    <span>{i.type}</span>
-                  </p>
-                  <p>
-                    <span>Gender: </span>
-                    <span>{i.gender}</span>
-                  </p>
-                  <p>
-                    <span>Origin:</span>
-                    <span> {i.origin}</span>
-                  </p>
-                  <img src={i.image} alt={i.name} width={200} height={200} />
-                </div>
-              );
-            })
-          : null}
+          {view === "episodes"
+            ? data.map((i) => {
+                return (
+                  <div className="card">
+                    <p>
+                      <span>id: </span>
+                      <span>{i.id}</span>
+                    </p>
+                    <p>
+                      <span>Name: </span>
+                      <span>{i.name}</span>
+                    </p>
+                    <p>
+                      <span>Air Date: </span>
+                      <span>{i.air_date}</span>
+                    </p>
+                    <p>
+                      <span>Episode: </span>
+                      <span>{i.episode}</span>
+                    </p>
+                    <p>
+                      <span>Season: </span>
+                      <span>{i.season}</span>
+                    </p>
+                  </div>
+                );
+              })
+            : null}
 
-        {view === "episodes"
-          ? data.map((i) => {
-              return (
-                <div className="card">
-                  <p>
-                    <span>id: </span>
-                    <span>{i.id}</span>
-                  </p>
-                  <p>
-                    <span>Name: </span>
-                    <span>{i.name}</span>
-                  </p>
-                  <p>
-                    <span>Air Date: </span>
-                    <span>{i.air_date}</span>
-                  </p>
-                  <p>
-                    <span>Episode: </span>
-                    <span>{i.episode}</span>
-                  </p>
-                  <p>
-                    <span>Season: </span>
-                    <span>{i.season}</span>
-                  </p>
-                </div>
-              );
-            })
-          : null}
-
-        {view === "locations"
-          ? data.map((i) => {
-              return (
-                <div className="card">
-                  <p>
-                    <span>id: </span>
-                    <span>{i.id}</span>
-                  </p>
-                  <p>
-                    <span>Name: </span>
-                    <span>{i.name}</span>
-                  </p>
-                  <p>
-                    <span>Type: </span>
-                    <span>{i.type}</span>
-                  </p>
-                  <p>
-                    <span>Dimension: </span>
-                    <span>{i.dimension}</span>
-                  </p>
-                </div>
-              );
-            })
-          : null}
-      </div>
+          {view === "locations"
+            ? data.map((i) => {
+                return (
+                  <div className="card">
+                    <p>
+                      <span>id: </span>
+                      <span>{i.id}</span>
+                    </p>
+                    <p>
+                      <span>Name: </span>
+                      <span>{i.name}</span>
+                    </p>
+                    <p>
+                      <span>Type: </span>
+                      <span>{i.type}</span>
+                    </p>
+                    <p>
+                      <span>Dimension: </span>
+                      <span>{i.dimension}</span>
+                    </p>
+                  </div>
+                );
+              })
+            : null}
+        </div>
+      ) : (
+        <div className="loading">Loading...</div>
+      )}
     </>
   );
 }
